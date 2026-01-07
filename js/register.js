107a8093fc51d933e96f06dd87d5e5ce16d3ae2e
bn4972477nandhini <bn4972477@gmail.com>
2026-01-07 10:33:58 +0530

    change

---
+++ js/register.js
@@ -0,0 +1,36 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();   
+
+  let fullname = document.getElementById("fullname").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let age = document.getElementById("age").value.trim();
+  let month = document.getElementById("month").value.trim();
+  let phone = document.getElementById("phone").value.trim();
+  let location = document.getElementById("location").value.trim();
+  let availability = document.getElementById("availability").value;
+
+  if (fullname === "") {
+    alert("Please enter your name");
+    return;
+  }
+  if (email === "") {
+    alert("Please enter your email");
+    return;
+  }
+  if (phone === "") {
+    alert("Please enter your phone number");
+    return;
+  }
+  if (location === "") {
+    alert("Please enter your location");
+    return;
+  }
+  if (availability === "") {
+    alert("Please select your availability");
+    return;
+  }
+  alert("Thank you for registering as a milk donor!");
+  form.reset();
+});
